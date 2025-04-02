83f6d48fc52e36b612f0ee4e01cf79f5a06b445f
Mkh <example@gmail.com>
2025-04-02 10:00:37 +0200

    add second page

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,9 @@
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import burgerMenu from './burgerMenu';
+import slider from "./slider";
+import tabs from './tabs';
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 import "/src/sass/style.scss";
 
-import burgerMenu from './burgerMenu';
-
-try {
-    new Swiper('.swiper', {
-        slidesPerView: 1,
-        loop: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: ".icon-right-open",
-            prevEl: ".icon-left-open",
-        },
-        breakpoints: {
-            // when window width is >= 1200px
-            1200: {
-                slidesPerView: 3,
-                spaceBetween: 5
-            },
-            1900: {
-                spaceBetween: 35
-            },
-        },
-        modules: [Navigation, Pagination],
-
-    });
-} catch (e) { }
-
 burgerMenu();
-
+slider();
+tabs();
